891a64fe6c74b5901617f47be5ba4b313df5d642
Rodrigo Silva <134323953+Rodrigo-s12@users.noreply.github.com>
2023-10-02 21:08:42 -0300

    Update ex01.js

--- aula 11/ex01.js
+++ aula 11/ex01.js
@@ -1,6 +1,6 @@
 var vel = 60
 console.log(`a velocidade so seu carro é  ${vel} km/h`)
-if (vel > 60) {
+if (vel > 60) { //condição simples
     console.log('você ultrapassou a velocidade permitida.MULTADO')
 }
 console.log(`dirija sempre usando cinto de segurança!`)
